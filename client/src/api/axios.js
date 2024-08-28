8719e0b0e43c29eb614bf1ce669f04a1477eb00c
Sergio Terrino <120554805+sergioterrino@users.noreply.github.com>
2024-08-28 12:01:47 +0200

    30 commit - update the baseUrlApi for renderUrlbackend

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
+// const instance = axios.create({
+//   baseURL: 'http://localhost:8000/api'
+// })
 const instance = axios.create({
-  baseURL: 'http://localhost:8000/api'
+  baseURL: 'https://stb-sortinghat.onrender.com/api'
 })
 
 // Esto es para enviar el token en el header de la petición, si existe
