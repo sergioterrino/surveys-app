366de5e0f17a7ff43f87fdd61d1d606282ef7831
Sergio Terrino <120554805+sergioterrino@users.noreply.github.com>
2024-08-28 22:32:12 +0200

    35 commit - pruebo import.meta.env.VITE_API_URL en vez de process.env

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const apiUrl = process.env.VITE_API_URL;
+const apiUrl = import.meta.env.VITE_API_URL;
 
 // const instance = axios.create({
 //   baseURL: 'http://localhost:8000/api'
